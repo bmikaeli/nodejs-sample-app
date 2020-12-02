1a343e530ed22d9ec4297cfdac680353fd252652
baptiste <baptiste.mikaelian@gmail.com>
2020-12-02 14:34:32 +0100

    new version for discord

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-
-const express = require('express')
-const app = express()
+fs = require('fs')
+const express = require('express');
+const app = express();
 const port = 3000
+
+const readFile = ((filePath) =>
+{
+	console.log("reading file ", filePath);
+        const data = fs.readFileSync(filePath, 'utf8')
+        console.log(data)
+        return data;
+});
 
 app.get('/', (req, res) => {
   console.log("Got new request");
-  res.send('Hello World!')
+  res.send('Contenu de l\'historique : ', readFile("/tmp/discord_history.txt"));
 })
 
 app.listen(port, () => {
